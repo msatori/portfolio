46b7fd092d8be04cc0ff6b09db3eed76692e1b4b
Maya Santiago <msatori.code@gmail.com>
2021-05-04 23:08:51 -0500

    created homepage, added router

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+
+export default function HomePage() {
+    return (
+        <section className="hero">
+            <div className="hero-wrapper"> 
+                <div className="hero-text">
+                    <h1>Maya Santiago</h1>
+                    <h3>Web Developer / UX Designer</h3>
+                </div>
+            </div>
+        </section>
+    )
+}
