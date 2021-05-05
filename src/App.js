8e1cb9caaf4827dac9bae34b9e3622f9429e5548
Maya Santiago <msatori.code@gmail.com>
2021-05-04 23:45:24 -0500

    basic hero styling

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
         </Switch>
       </Router>
 
-      <Bio />
     </main>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import { React } from 'react';
-
+import bg from '../../assets/images/flower.png';
 
 export default function HomePage() {
     return (
         <section className="hero">
             <div className="hero-wrapper"> 
                 <div className="hero-text">
+                    <img src={bg} />
                     <h1>Maya Santiago</h1>
                     <h3>Web Developer / UX Designer</h3>
                 </div>
