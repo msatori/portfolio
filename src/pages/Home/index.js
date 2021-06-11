5c4a1af0b4882b783565f22ab8a8b01edf1c09c5
Maya Santiago <msatori.code@gmail.com>
2021-06-11 18:45:45 -0500

    swapped bg images

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,17 @@
 import { React } from 'react';
-import bg from '../../assets/images/flower.png';
 
 export default function HomePage() {
     return (
         <section className="hero">
-            <div className="hero-wrapper"> 
-                <div className="hero-text">
-                    <img src={bg} />
-                    <h1>Maya Santiago</h1>
-                    <h3>Web Developer / UX Designer</h3>
+            <div className="hero-wrapper">
+                <div className="hero-img">
+                    <div className="hero-text">
+                        <h1>Maya Santiago</h1>
+                    </div>
+
                 </div>
+
             </div>
         </section>
     )
-}
+}    
