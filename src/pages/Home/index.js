9d0305ff2ec42364cd4ee08f716d2a4974edcdc4
Maya Santiago <msatori.code@gmail.com>
2021-06-11 19:29:42 -0500

    changed font, centered photo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,14 +4,12 @@ export default function HomePage() {
     return (
         <section className="hero">
             <div className="hero-wrapper">
-                <div className="hero-img">
-                    <div className="hero-text">
-                        <h1>Maya Santiago</h1>
-                    </div>
-
+                <div className="hero-img" >
+                <div className="hero-text">
+                    <h1>Maya <br /> Santiago</h1>
                 </div>
-
+                </div>
             </div>
         </section>
     )
-}    
+}
